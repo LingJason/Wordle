38179eb0e77e600ad1604e2893f005cca20efc0d
Jason Ling <jasonling1996@gmail.com>
2022-12-15 15:25:08 -0800

    Add validation when submit answer

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -8,7 +8,7 @@ export const useWordle = (solution) => {
   const [correct, setCorrect] = useState(false);
 
   const formatGuess = () => {
-
+    console.log("here", currentGuess)
   }
 
   const addNewGuess = () => {
@@ -34,7 +34,30 @@ export const useWordle = (solution) => {
         })
       }
     }
+  
+    if (key === 'Enter') {
+      //Check if turn is greater than 5
+      if (turn > 5) {
+        console.log('You used all your guesses')
+        return
+      }
+
+      //Check if word is 5 characters
+      if (currentGuess.length !== 5) {
+        console.log('Guess is invalid length')
+        return
+      }
+
+      //Check if word already exist
+      if (history.includes(currentGuess)) {
+        console.log('Word already was guessed')
+        return
+      }
+      formatGuess()
+    }
+
   }
+
 
   return {
     turn, currentGuess, guesses, correct, handleKeyUp
