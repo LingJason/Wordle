2b2f8fd5a3af3d30ed273e17bb822c788edc3e5a
Jason Ling <jasonling1996@gmail.com>
2022-12-16 17:39:15 -0800

    Add function to check if guess is correct, add to history and increase count

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -2,13 +2,17 @@ import React, { useEffect } from 'react'
 import { useWordle } from '../hooks/useWordle'
 
 export default function Wordle({answer}) {
-  const { currentGuess, handleKeyUp } = useWordle(answer)
+  const { currentGuess, guesses, turn, correct, handleKeyUp } = useWordle(answer)
 
   useEffect(() => {
     window.addEventListener('keyup', handleKeyUp)
 
     return() => window.removeEventListener('keyup', handleKeyUp)
   }, [handleKeyUp])
+
+  useEffect(() => {
+    console.log(guesses, turn, correct)
+  }, [guesses, turn, correct])
 
   return (
     <div>Current Guess - {currentGuess} </div>
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 export const useWordle = (answer) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState('');
-  const [guesses, setGuesses] = useState([]);
+  const [guesses, setGuesses] = useState([...Array(6)]);
   const [history, setHistory] = useState([]);
   const [correct, setCorrect] = useState(false);
 
@@ -37,9 +37,25 @@ export const useWordle = (answer) => {
     return formattedGuess
   }
 
-  const addNewGuess = () => {
-
-
+  const addNewGuess = (formattedGuess) => {
+     // Check if answer is correct
+    if (currentGuess === answer) {
+      setCorrect(true)
+    }
+    setGuesses((prevGuesses) => {
+      let newGuesses = [...prevGuesses]
+      newGuesses[turn] = formattedGuess
+      return newGuesses
+    })
+    setHistory((prevHistory) => {
+        //Add guess to guess state
+      return [...prevHistory, currentGuess]
+    })
+    // Add one to the turn state
+    setTurn((prevTurn) => {
+      return prevTurn + 1;
+    })
+    setCurrentGuess('')
   }
 
   const handleKeyUp = ( {key}) => {
@@ -80,7 +96,7 @@ export const useWordle = (answer) => {
         return
       }
       const formatted = formatGuess()
-      console.log(formatted)
+      addNewGuess(formatted)
     }
 
   }
